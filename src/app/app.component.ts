a1ff0bc8f6cea64af83c7896361dc5e3e3a6c13b
djomlaks <mladenristic1997@gmail.com>
2018-09-01 14:48:45 +0200

    frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  greetings: string[] = [];
+  showConversation: boolean = false;
+  ws: any;
+  name: string;
+  disabled: boolean;
+
+  constructor(){}
+
+  connect() {
+    //connect to stomp where stomp endpoint is exposed
+    //let withWS = new SockJS("http://localhost:8080/greeting");
+    //if we want to use SockJS then in WebSocketConfig add withSockJS(); in Spring
+    let socket = new WebSocket("ws://localhost:8080/greeting");
+    this.ws = Stomp.over(socket);
+    let that = this;
+    this.ws.connect({}, function(frame) {
+      that.ws.subscribe("/errors", function(message) {
+        alert("Error " + message.body);
+      });
+      that.ws.subscribe("/topic/reply", function(message) {
+        console.log(message)
+        that.showGreeting(message.body);
+      });
+      that.disabled = true;
+    }, function(error) {
+      alert("STOMP error " + error);
+    });
+  }
+
+  disconnect() {
+    if (this.ws != null) {
+      this.ws.ws.close();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
+  }
+
+  sendName() {
+    let data = JSON.stringify({
+      'name' : this.name
+    })
+    this.ws.send("/app/message", {}, data);
+  }
+
+  showGreeting(message) {
+    this.showConversation = true;
+    this.greetings.push(message)
+  }
+
+  setConnected(connected) {
+    this.disabled = connected;
+    this.showConversation = connected;
+    this.greetings = [];
+  }
+}
